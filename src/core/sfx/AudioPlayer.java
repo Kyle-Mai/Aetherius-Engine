3303b62102ed5dc95fac2400de9d0393ec10f203
Zephyrai <notkylemai@gmail.com>
2017-07-12 03:31:20 -0400

    Testing new (theoretically) optimized version of the AudioPlayer.
    Compared to the old one, the new one should;
    
    - Allow the user to determine if audio is playing in the player or not.
    
    - Correctly play sequential audio without overlapping.
    
    - More efficiently use memory and dump unneeded information.
    
    - Have less risk of memory leaks (as Thread is more dangerous to use for this sort of operation)
    
    - More effectively prevent major errors by throwing exceptions where necessary.

--- src/core/sfx/AudioPlayer.java
+++ src/core/sfx/AudioPlayer.java
@@ -3,11 +3,16 @@ package core.sfx;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import sun.misc.JavaNioAccess;
+import sun.nio.ch.DirectBuffer;
+import sun.nio.ch.Interruptible;
 
+import javax.accessibility.Accessible;
+import javax.accessibility.AccessibleTable;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
+import java.io.Flushable;
+import java.util.*;
+import java.util.function.BooleanSupplier;
 
 /*
 Lolita's Revenge
@@ -16,19 +21,21 @@ June 29 2017
 Plays and manages audio files.
  */
 
-public class AudioPlayer extends Thread {
+public class AudioPlayer extends ArrayList<Media> implements Runnable {
 
     /*------------------------------------------------------------------------------------------------------------------
      Variables.
      Define the function of the SFX player.
      */
 
+    private final long sleepDuration = 600; //How long the thread's refresh rate is while audio is playing
+
     private double delay = 0;
     private boolean loop = false;
+    private boolean playing = false;
     private boolean shuffle = false;
     private MediaPlayer mediaPlayer;
     private ArrayList<File> audioFile = new ArrayList<>();
-    private ArrayList<Media> audio = new ArrayList<>();
     static { JFXPanel fxPanel = new JFXPanel(); } //needed to play the audio
 
     /*------------------------------------------------------------------------------------------------------------------
@@ -38,17 +45,17 @@ public class AudioPlayer extends Thread {
 
     public AudioPlayer() {}
 
-    public AudioPlayer(File audio) {audioFile.add(audio); }
+    public AudioPlayer(File a) { audioFile.add(a); }
 
-    public AudioPlayer(File audio, double volume) {
-        audioFile.add(audio);
-        setVolume(volume);
+    public AudioPlayer(File a, double v) {
+        audioFile.add(a);
+        setVolume(v);
     }
 
-    public AudioPlayer(File audio, double volume, double delay) {
-        audioFile.add(audio);
-        setVolume(volume);
-        this.delay = delay;
+    public AudioPlayer(File a, double v, double d) {
+        audioFile.add(a);
+        setVolume(v);
+        delay = d;
     }
 
     /*------------------------------------------------------------------------------------------------------------------
@@ -59,42 +66,51 @@ public class AudioPlayer extends Thread {
     public void addFile(File... file) { audioFile.addAll(Arrays.asList(file)); } //adds a new audio file to the sfx player
     public void removeFile(File f) { audioFile.remove(f); } //removes a file
     public void removeFile(int i) { audioFile.remove(i); } //removes a file from the selected index
+    public void dumpFiles() { audioFile.clear(); }
 
     public File getFile(File f) { return audioFile.get(audioFile.indexOf(f)); } //gets a file
     public File getFile(int i) { return audioFile.get(i); } //gets a file from an index
     public int getFileIndex(File f) { return audioFile.indexOf(f); } //gets the index of a file
 
     public void setVolume(double volume) {mediaPlayer.setVolume(0.01 * volume); } //sets the volume of the audio player
-    public void setDelay(double delay) { this.delay = delay; } //sets the delay between initialization and playing
+    public void setDelay(double d) { delay = d; } //sets the delay between initialization and playing
 
     public double getVolume() { return mediaPlayer.getVolume() * 100; } //gets the volume of the audio player
     public double getDuration() { return mediaPlayer.getTotalDuration().toMillis(); } //gets the duration of the current audio
     public double getDelay() { return delay; } //gets the currently selected delay
 
-    public void loop() { mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE); } //loops the audio
+    public void loop() { //loops the audio
+        audioLoop();
+        loop = true;
+    }
+
+    public boolean isPlaying() { return playing; }
     public boolean isLooping() { return loop; } //whether or not the audio is looping
-    public void shuffle() { this.shuffle = true; } //shuffles the audio
+    public void setShuffle(boolean b) { shuffle = b; } //shuffles the audio
     public boolean isShuffling() { return shuffle; } //whether or not the audio is being shuffled
 
     public void stopAudio() { mediaPlayer.stop(); } //stops the audio
 
     @Override
     public void run() { //plays the audio
+        if (playing) throw new RuntimeException("Invalid operation - Audio thread was accessed during execution."); //Safety net.
+
         initializeData();
+        mediaPlayerSetup();
 
-        if (loop && !shuffle && audio.size() <= 1) { loop(); }
+        if (loop && !shuffle && size() <= 1) { audioLoop(); }
 
-        if (audio.size() == 1) {
-            loadAudio(audio.get(0));
-        } else if (audio.size() > 1 && shuffle) {
+        if (size() == 1) {
+            loadAudio(get(0));
+        } else if (size() > 1 && shuffle) {
             shuffleAudio();
-        } else if (audio.size() > 1 && loop) {
+        } else if (size() > 1 && !shuffle && !loop) {
             playList();
         } else {
-            System.out.println("[SFX](run) Unexpected Error - Audio initialization was incorrect.");
+            throw new IllegalArgumentException("Unexpected Error - Audio initialization was incorrect.");
         }
 
-        currentThread().interrupt(); //closes the thread down
+        Thread.currentThread().interrupt(); //closes the thread down
     }
 
     /*------------------------------------------------------------------------------------------------------------------
@@ -102,40 +118,62 @@ public class AudioPlayer extends Thread {
      Methods used by the run function when the audio is set to play. These should NOT be touched or accessed.
      */
 
-    private void initializeData() { //initializes the audio files from the data
-        audio.clear();
+    private void mediaPlayerSetup() {
+        mediaPlayer.setOnEndOfMedia(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Audio ended.");
+                playing = false;
+                Thread.currentThread().interrupt();
+            }
+        });
+    }
 
+    private void audioLoop() { mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE); }
+
+    private void initializeData() { //initializes the audio files from the data
+        clear();
         if (audioFile.size() == 0) {
-            System.out.println("[SFX](initData) Unexpected Error - No audio loaded. Discarding audio player.");
-            currentThread().interrupt();
+            throw new NullPointerException("Unexpected Error - No audio loaded.");
         } else {
             for (int i = 0; i < audioFile.size(); i++) {
-                audio.add(new Media(audioFile.get(i).toString()));
+                add(new Media(audioFile.get(i).toString()));
             }
         }
+        audioFile.clear(); //All audio files initialized, might as well dump the now useless data
     }
 
     private void loadAudio(Media media) { //loads the audio into the media player and plays it after the delay (if one was set)
         mediaPlayer = new MediaPlayer(media);
         try {
-            sleep((long)delay);
+            Thread.sleep((long)delay);
         } catch (InterruptedException e) {
-            currentThread().interrupt();
+            Thread.currentThread().interrupt();
         }
         mediaPlayer.play();
+        playing = true;
+
+        while(playing) {
+            try {
+                Thread.currentThread().sleep(sleepDuration);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 
     private void shuffleAudio() { //selects a random audio from the list and plays it, looping when applicable
         Random shuffler = new Random();
-        loadAudio(audio.get(shuffler.nextInt(audio.size() - 1)));
+        loadAudio(get(shuffler.nextInt(size() - 1)));
         if (loop) { //if the program is set to loop, choose another audio file after the current one finishes playing
             shuffleAudio();
         }
     }
 
     private void playList() { //plays the audio in order
-        for (int i = 0; i < audio.size(); i++) {
-            loadAudio(audio.get(i));
+        for (int i = 0; i < size(); i++) {
+            loadAudio(get(i));
         }
     }
 
